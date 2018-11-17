feba73cac2ac44d873b05785bf5b8cbcff7370e0
Jake Mortimer <mortimer907@gmail.com>
2018-11-17 16:44:41 +0000

    JSONIFIED

--- DurHack/basicInfo.py
+++ DurHack/basicInfo.py
@@ -6,15 +6,16 @@ for x in data['entry']:
 	b = []
 	y = x['resource']['resourceType']
 	if y == "Patient":
-		for z in range(2):
-			b.append(x['resource']['extension'][z]['valueCodeableConcept']['coding'][0]['display'])
+		b.append(x['resource']['extension'][0]['valueCodeableConcept']['coding'][0]['display'])
+		b.append(x['resource']['extension'][1]['valueCodeableConcept']['coding'][0]['display'])
 		b.append(x['resource']['extension'][2]['valueAddress'])
 		b.append(x['resource']['extension'][3]['valueString'])
 		b.append(x['resource']['extension'][4]['valueCode'])
-		b.append(x['resource']['name'])
+		b.append(x['resource']['name'][0])
 		b.append(x['resource']['gender'])
 		b.append(x['resource']['birthDate'])
 		b.append(x['resource']['address'][0]['line'])
-		bI.append(b)
+		b1 = {"race": b[0], "ethnicity": b[1], "city": b[2], "maidenNames": b[3], "birthSex": b[4], "name": b[5], "gender": b[6], "birthDate": b[7], "address": b[8]}
+		b1 = json.dumps(b1)
+		bI.append(b1)
 print(bI)
-		
