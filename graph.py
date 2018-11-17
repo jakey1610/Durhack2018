36121acb4222d580ed15f5f51f2b1dfa58fd0249
Jake Mortimer <jake.a.mortimer@durham.ac.uk>
2018-11-17 23:45:51 +0000

    Changes made to graph

--- graph.py
+++ graph.py
@@ -5,24 +5,31 @@ import matplotlib.pyplot as plt
 freq = {}
 xcoords = []
 ycoords = []
-
+failed = []
 for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
 	if '.json' in file:
 		data = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/' + file))
 
-		age = file.split('_')[2].split('.')[0]
 		for x in data['entry']:
 			y = x['resource']['resourceType']
-			if y == 'Condition':
-				if 'cancer' in x['resource']['code']['coding'][0]['display'].lower():
-					try:
-						freq[age] +=1
-					except KeyError:
-						freq[age] = 1
+			if y == 'Observation':
+				try:
+					if "systolic" in x['resource']['component'][0]['code']['text'].lower():
+						xcoords.append(x['resource']['component'][0]['valueQuantity']['value'])
+				except KeyError:
+					#print(file + ": Fuck off!")
+					failed.append(file)
+		for x in data['entry']:
+			y = x['resource']['resourceType']
+			if y == 'Observation':
+				try:
+					if "hemoglobin" in x['resource']['code']['coding'][0]['display'].lower():
+						ycoords.append(x['resource']['valueQuantity']['value'])
+				except KeyError:
+					#print(file + ": Fuck off!")
+					failed.append(file)
 
-for i in freq.keys():
-	xcoords.append(float(i))
-	ycoords.append(float(freq[i]))
 
-plt.plot(xcoords, ycoords, 'ro')
+print(failed)
+plt.plot(xcoords, ycoords)
 plt.show()
