013de0b9356e36727e44ca9aa730f05cf0141ccb
sami-t1 <45122693+sami-t1@users.noreply.github.com>
2018-11-17 23:55:26 +0000

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 import matplotlib.pyplot as plt
@@ -9,13 +10,14 @@ failed = []
 for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
 	if '.json' in file:
 		data = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/' + file))
-
 		for x in data['entry']:
+			count1 = 0
 			y = x['resource']['resourceType']
 			if y == 'Observation':
 				try:
 					if "systolic" in x['resource']['component'][0]['code']['text'].lower():
 						xcoords.append(x['resource']['component'][0]['valueQuantity']['value'])
+						count1 += 1
 				except KeyError:
 					#print(file + ": Fuck off!")
 					failed.append(file)
@@ -23,11 +25,13 @@ for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
 			y = x['resource']['resourceType']
 			if y == 'Observation':
 				try:
-					if "hemoglobin" in x['resource']['code']['coding'][0]['display'].lower():
+					if "hemoglobin" in x['resource']['code']['coding'][0]['display'].lower() and count1 == 1:
 						ycoords.append(x['resource']['valueQuantity']['value'])
 				except KeyError:
 					#print(file + ": Fuck off!")
 					failed.append(file)
+				if count1 = 0:
+					del xcoords[-1]
 
 
 print(failed)
