ceb23d7cfb8099aae64ac007e982afc57285bb75
Jake Mortimer <jake.a.mortimer@durham.ac.uk>
2018-11-17 20:14:05 +0000

    .

--- DurHack/search.py
+++ DurHack/search.py
@@ -1,10 +1,9 @@
+import os
 def search(x):
 	users = [] # All the filenames that include 
 	direc = '.'
-	import os
 	filenames = os.listdir(direc)
 	for name in filenames:
 		if x in name:
 			users.append(name)
 	return users
-print(search("y"))
