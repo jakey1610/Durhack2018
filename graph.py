6f65f60ccdeee1508e610e2872e978b6efac80bb
Jake Mortimer <jake.a.mortimer@durham.ac.uk>
2018-11-18 01:55:46 +0000

    More Graphs, change path

--- graph.py
+++ graph.py
@@ -2,38 +2,131 @@
 import json
 import os
 import matplotlib.pyplot as plt
+def graphChol(person):
+	freq = {}
+	xcoords = []
+	ycoords = []
+	failed = []
+	c = 0
+	for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
+		if '.json' in file:
+			data = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/' + file))
+			
+			for x in data['entry']:
+				y = x['resource']['resourceType']
+				if y == 'Observation':
+					try:
+						if "total cholesterol" in x['resource']['code']['coding'][0]['display'].lower():
+							ycoords.append(x['resource']['valueQuantity']['value'])
+							xcoords.append(c)
+							c +=1 
+					except KeyError:
+						#print(file + ": Fuck off!")
+						failed.append(file)
+	ycoords.sort()
+	data2 = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/'+person))
+	for x in data2['entry']:
+		y = x['resource']['resourceType']
+		if y == 'Observation':
+			try:
+				if "total cholesterol" in x['resource']['code']['coding'][0]['display'].lower():
+					z = x['resource']['valueQuantity']['value']
+			except KeyError:
+				print("LIFE")
+	plt.annotate(
+	# Label and coordinate
+	'Patient is here', xy=(ycoords.index(z), z), xytext=(ycoords.index(z), z+100),
+	 
+	# Custom arrow
+	arrowprops=dict(facecolor='black', shrink=0.05)
+	)
 
-freq = {}
-xcoords = []
-ycoords = []
-failed = []
-for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
-	if '.json' in file:
-		data = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/' + file))
-		for x in data['entry']:
-			count1 = 0
-			y = x['resource']['resourceType']
-			if y == 'Observation':
-				try:
-					if "systolic" in x['resource']['component'][0]['code']['text'].lower():
-						xcoords.append(x['resource']['component'][0]['valueQuantity']['value'])
-						count1 += 1
-				except KeyError:
-					#print(file + ": Fuck off!")
-					failed.append(file)
-		for x in data['entry']:
-			y = x['resource']['resourceType']
-			if y == 'Observation':
-				try:
-					if "hemoglobin" in x['resource']['code']['coding'][0]['display'].lower() and count1 == 1:
-						ycoords.append(x['resource']['valueQuantity']['value'])
-				except KeyError:
-					#print(file + ": Fuck off!")
-					failed.append(file)
-				if count1 = 0:
-					del xcoords[-1]
 
+	
+	plt.bar(xcoords, ycoords, 1/1.5,color="blue")
+	plt.savefig('gCol.png')
 
-print(failed)
-plt.plot(xcoords, ycoords)
-plt.show()
+def graphSodium(person):
+	freq = {}
+	xcoords = []
+	ycoords = []
+	failed = []
+	c = 0
+	for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
+		if '.json' in file:
+			data = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/' + file))
+			
+			for x in data['entry']:
+				y = x['resource']['resourceType']
+				if y == 'Observation':
+					try:
+						if "sodium" in x['resource']['code']['coding'][0]['display'].lower():
+							ycoords.append(x['resource']['valueQuantity']['value'])
+							xcoords.append(c)
+							c +=1 
+					except KeyError:
+						#print(file + ": Fuck off!")
+						failed.append(file)
+	ycoords.sort()
+	data2 = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/'+person))
+	for x in data2['entry']:
+		y = x['resource']['resourceType']
+		if y == 'Observation':
+			try:
+				if "sodium" in x['resource']['code']['coding'][0]['display'].lower():
+					z = x['resource']['valueQuantity']['value']
+			except KeyError:
+				print("LIFE")
+	plt.annotate(
+	# Label and coordinate
+	'Patient is here', xy=(ycoords.index(z), z), xytext=(ycoords.index(z), z+10),
+	 
+	# Custom arrow
+	arrowprops=dict(facecolor='black', shrink=0.05)
+	)
+	plt.axis([0,xcoords[-1],50,155])
+	plt.bar(xcoords, ycoords, 1/1.5,color="red")
+	plt.savefig('gSod.png')
+
+def graphCalc(person):
+	freq = {}
+	xcoords = []
+	ycoords = []
+	failed = []
+	c = 0
+	for file in os.listdir('C:/Users/Jake Mortimer/Downloads/fhir'):
+		if '.json' in file:
+			data = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/' + file))
+			
+			for x in data['entry']:
+				y = x['resource']['resourceType']
+				if y == 'Observation':
+					try:
+						if "calcium" in x['resource']['code']['coding'][0]['display'].lower():
+							ycoords.append(x['resource']['valueQuantity']['value'])
+							xcoords.append(c)
+							c +=1 
+					except KeyError:
+						#print(file + ": Fuck off!")
+						failed.append(file)
+	ycoords.sort()
+	data2 = json.load(open('C:/Users/Jake Mortimer/Downloads/fhir/'+person))
+	for x in data2['entry']:
+		y = x['resource']['resourceType']
+		if y == 'Observation':
+			try:
+				if "calcium" in x['resource']['code']['coding'][0]['display'].lower():
+					z = x['resource']['valueQuantity']['value']
+			except KeyError:
+				print("LIFE")
+	plt.annotate(
+	# Label and coordinate
+	'Patient is here', xy=(ycoords.index(z), z), xytext=(ycoords.index(z)-400, z+1.5),
+	 
+	# Custom arrow
+	arrowprops=dict(facecolor='black', shrink=0.05)
+	)
+	plt.axis([0,xcoords[-1],5,12])
+	plt.bar(xcoords, ycoords, 1/1.5,color="green")
+	plt.savefig('gCal.png')
+graphCalc("Abbott701_Veronika555_74.json")
